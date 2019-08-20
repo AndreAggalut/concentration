750dc37173eefe9e8769daa3086956d23cf85c99
AndreAggalut <aaggalut3895@gmail.com>
2019-08-20 15:37:40 -0700

    start button added

--- index.js
+++ index.js
@@ -1,48 +1,52 @@
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
-const emojis = ['😀', '💩', '🍆', '🌮', '🔥', '💯', '🍿', '🍑'];
-const pairs = emojis.concat(...emojis);
-const shuffled = pairs.sort(() => Math.random() - 0.5);
-console.log(shuffled);
-const cards = document.querySelectorAll('.card');
+function startGame() {
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+  const emojis = ['😀', '💩', '🍆', '🌮', '🔥', '💯', '🍿', '🍑'];
+  const pairs = emojis.concat(...emojis);
+  const shuffled = pairs.sort(() => Math.random() - 0.5);
+  console.log(shuffled);
+  const cards = document.querySelectorAll('.card');
 
-// document.addEventListener('keydown', event => console.log(event));
+  // document.addEventListener('keydown', event => console.log(event));
 
-cards.forEach((card, i) => {
-  // card.innerHTML = shuffled[i];
-  card.setAttribute('data-value', shuffled[i]);
-  card.setAttribute('data-key', card.innerHTML);
+  cards.forEach((card, i) => {
+    // card.innerHTML = shuffled[i];
+    card.setAttribute('data-value', shuffled[i]);
+    card.setAttribute('data-key', card.innerHTML);
 
-  // click event listener
-  card.addEventListener('click', () => {
-    /*     card.classList.toggle('face-down');
+    // click event listener
+    card.addEventListener('click', () => {
+      /*     card.classList.toggle('face-down');
     card.innerHTML = card.dataset.value;
     console.log(card.innerHTML); */
-    flipFaceUp(card);
-    setTimeout(() => {
-      flipFaceDown(card);
-    }, 1500);
-  });
-
-  // key event listener
-  document.addEventListener('keydown', event => {
-    if (event.key.toUpperCase() === card.dataset.key) {
-      console.log('event.key:', event.key);
-      console.log('dataset:', card.dataset.key.toUpperCase());
       flipFaceUp(card);
       setTimeout(() => {
         flipFaceDown(card);
       }, 1500);
-    }
+    });
+
+    // key event listener
+    document.addEventListener('keydown', event => {
+      if (event.key.toUpperCase() === card.dataset.key) {
+        console.log('event.key:', event.key);
+        console.log('dataset:', card.dataset.key.toUpperCase());
+        flipFaceUp(card);
+        setTimeout(() => {
+          flipFaceDown(card);
+        }, 1500);
+      }
+    });
   });
-});
 
-function flipFaceUp(card) {
-  card.classList.toggle('face-down');
-  card.innerHTML = card.dataset.value;
-  console.log(card.innerHTML);
-}
+  function flipFaceUp(card) {
+    card.classList.toggle('face-down');
+    card.innerHTML = card.dataset.value;
+    console.log(card.innerHTML);
+  }
 
-function flipFaceDown(card) {
-  card.classList.toggle('face-down');
-  card.innerHTML = card.dataset.key;
+  function flipFaceDown(card) {
+    card.classList.toggle('face-down');
+    card.innerHTML = card.dataset.key;
+  }
 }
+const startButton = document.querySelector('button');
+startButton.addEventListener('click', () => startGame());
