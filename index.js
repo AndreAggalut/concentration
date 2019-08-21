c4c039a47af40d82f038e8285473632c7db5c5b7
emily-yang <cigoua@gmail.com>
2019-08-21 15:53:19 -0700

    start game logic

--- index.js
+++ index.js
@@ -1,52 +1,70 @@
+const faceUpCards = [];
+
 function startGame() {
-  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+  /* Generate array of emoji pairs in random order */
   const emojis = ['😀', '💩', '🍆', '🌮', '🔥', '💯', '🍿', '🍑'];
   const pairs = emojis.concat(...emojis);
   const shuffled = pairs.sort(() => Math.random() - 0.5);
   console.log(shuffled);
-  const cards = document.querySelectorAll('.card');
 
-  // document.addEventListener('keydown', event => console.log(event));
+  /* Initialize score */
+  const matchCount = 0;
+
+  const cards = document.querySelectorAll('.card');
 
   cards.forEach((card, i) => {
     // card.innerHTML = shuffled[i];
+    const sameCard = card;
     card.setAttribute('data-value', shuffled[i]);
     card.setAttribute('data-key', card.innerHTML);
 
     // click event listener
-    card.addEventListener('click', () => {
-      /*     card.classList.toggle('face-down');
-    card.innerHTML = card.dataset.value;
-    console.log(card.innerHTML); */
-      flipFaceUp(card);
-      setTimeout(() => {
-        flipFaceDown(card);
-      }, 1500);
-    });
+    card.addEventListener('click', handleCardClick);
 
-    // key event listener
-    document.addEventListener('keydown', event => {
-      if (event.key.toUpperCase() === card.dataset.key) {
-        console.log('event.key:', event.key);
-        console.log('dataset:', card.dataset.key.toUpperCase());
-        flipFaceUp(card);
-        setTimeout(() => {
-          flipFaceDown(card);
-        }, 1500);
-      }
-    });
+    // // key event listener
+    // document.addEventListener('keydown', (event, sameCard) => handleCardKeydown(event, sameCard));
   });
+}
 
-  function flipFaceUp(card) {
-    card.classList.toggle('face-down');
-    card.innerHTML = card.dataset.value;
-    console.log(card.innerHTML);
-  }
+function handleCardClick(event) {
+  // if at least 2 cards are already flipped up, do nothing
+  if (faceUpCards.length >= 2) return;
 
-  function flipFaceDown(card) {
-    card.classList.toggle('face-down');
-    card.innerHTML = card.dataset.key;
+  flipFaceUp(this);
+  this.disabled = true;
+  const cardValue = this.dataset.value;
+  faceUpCards.push(cardValue);
+
+  if (faceUpCards.length === 1) {
+    setTimeout(() => {
+      flipFaceDown(this);
+      this.removeAttribute('disabled');
+      faceUpCards.length = 0;
+    }, 2000);
   }
 }
-const startButton = document.querySelector('button');
-startButton.addEventListener('click', () => startGame());
+
+/* function handleCardKeydown(event, card) {
+  console.log(event);
+  console.log(card);
+  if (event.key.toUpperCase() === card.dataset.key) {
+    flipFaceUp(card);
+    setTimeout(() => {
+      flipFaceDown(card);
+    }, 2000);
+  }
+} */
+
+function flipFaceUp(card) {
+  card.classList.toggle('face-down');
+  card.innerHTML = card.dataset.value;
+}
+
+function flipFaceDown(card) {
+  card.classList.toggle('face-down');
+  card.innerHTML = card.dataset.key;
+}
+
+startGame();
+/* const startButton = document.querySelector('button');
+startButton.addEventListener('click', () => startGame()); */
