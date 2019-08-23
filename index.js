7b1797e2a86323907e460b8d3a1ed152dd1ec0e1
emily-yang <cigoua@gmail.com>
2019-08-22 19:16:57 -0700

    edit some comments

--- index.js
+++ index.js
@@ -12,37 +12,36 @@ function startGame() {
   const shuffled = pairs.sort(() => Math.random() - 0.5);
   console.log(shuffled);
 
-  /* Initialize score here */
+  /* Initialize score */
   matchCount = 0;
 
   const cards = document.querySelectorAll('.card');
-
   cards.forEach((card, i) => {
-    // card.innerHTML = shuffled[i];
-    const sameCard = card;
     card.setAttribute('data-value', shuffled[i]);
     card.setAttribute('data-key', card.innerHTML);
 
+    // attach click and key event listeners
     card.addEventListener('click', event => handleInput(event, card));
     document.addEventListener('keydown', event => handleInput(event, card));
   });
 }
 
 function handleInput(event, card) {
+  // do nothing if already two cards flipped up OR same card is doubleclicked
   if (faceUpCards.length >= 2 || (faceUpCards.length !== 0 && card === faceUpCards[0])) return;
 
   if (event.type === 'click' || event.key.toUpperCase() === card.dataset.key) {
     flipFaceUp(card);
 
     if (faceUpCards.length === 1) {
-      // first flipped card
+      // for first flipped card
       firstCardTimer = setTimeout(() => flipFaceDown(card), SINGLE_FLIP_DURATION);
     } else {
-      // second flipped card
+      // for second flipped card
 
       // cancel timer for handling single card flip
       clearTimeout(firstCardTimer);
-      // set new timer to show flipped up cards for short period
+      // start a new timer to show flipped up cards for short period
       setTimeout(() => {
         const [card1Value, card2Value] = [faceUpCards[0].dataset.value, faceUpCards[1].dataset.value];
         if (card1Value === card2Value) handleCardsMatch(faceUpCards);
